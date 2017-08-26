166cf5f93bbc9c88584591e31f8152a052682d6e
Ivan <ivanbasurman88@yandex.ru>
2017-08-26 21:33:03 +0300

    0.1.1

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,27 +9,11 @@ def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
-    pygame.display.set_caption("Allien  Invasion")
+    pygame.display.set_caption("Allien")
     ship = Ship(screen)
 
     while True:
-        # gf.check_events(ship)
-        for event in pygame.event.get():
-            print(event.type)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    ship.rect.centerx += 10
-
-                elif event.key == pygame.K_LEFT:
-                    ship.rect.centerx -= 10
-
-                elif event.key == pygame.K_DOWN:
-                    ship.rect.centery -= 10
-
-                elif event.key == pygame.K_SPACE:
-                    ship.rect.centerx += 10
-
+        gf.check_events(ship)
         gf.update_screen(ai_settings, screen, ship)
-
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -4,10 +4,8 @@ import pygame
 
 def check_events(ship):
     for event in pygame.event.get():
-        ship.rect.centery -= 10
-        ship.rect.centerx += 10
-        # if event.type == pygame.QUIT:
-        #    sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
@@ -17,10 +15,10 @@ def check_events(ship):
                 ship.rect.centerx -= 10
 
             elif event.key == pygame.K_DOWN:
-                ship.rect.centery -= 10
+                ship.rect.centery += 10
 
-            elif event.key == pygame.K_SPACE:
-                ship.rect.centerx += 10
+            elif event.key == pygame.K_UP:
+                ship.rect.centery -= 12
 
 
 def update_screen(ai_settings, screen, ship):
