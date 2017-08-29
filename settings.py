5d78803797a5fd5a7b9eafbd64e141e1e2d70fbc
Ivan <ivanbasurman88@yandex.ru>
2017-08-29 17:52:03 +0300

    0.3.2

--- settings.py
+++ settings.py
@@ -3,12 +3,12 @@ class Settings:
     def __init__(self):
         self.screen_width = 800
         self.screen_height = 600
-        # self.big_color = (230, 230, 230)
+        self.big_color = (230, 230, 230)
         self.ship_speed_factor = 2.75
-        self.bullet_speed_factor = 3
+        self.bullet_speed_factor = 3.5
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        # self.bullet_color = 60, 60, 60
 
 
 
